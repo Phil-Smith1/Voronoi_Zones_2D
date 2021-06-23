6aabb5bbe6844c6379bc260a9eeea1318ca49d06
Phil-Smith1 <pvsmith100@btinternet.com>
2021-06-23 18:19:28 +0100

    Update

--- Voronoi_Zones_2D/Voronoi_Zones_2D_Main.cpp
+++ Voronoi_Zones_2D/Voronoi_Zones_2D_Main.cpp
@@ -1,14 +1,8 @@
-//
-//  main.cpp
-//  Voronoi_Zones_2D
-//
-//  Created by Phil Smith on 23/06/2021.
-//
+
 
 #include <iostream>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
     std::cout << "Hello, World!\n";
     return 0;
 }
